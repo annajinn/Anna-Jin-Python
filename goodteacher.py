c341cc2e234bbc9cbd6bd9b7a2e11593867dde7b
annajinn <anna.jinn2@fake.com>
2021-10-20 12:43:04 +1100

    goodteacher code

---
+++ goodteacher.py
@@ -0,0 +1,13 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Anna                                 |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+x="Mr Horan"
+y=input("Teacher's name (try Mr Horan) ")
+if y==x:
+    print("You are lucky, he is a great teacher.")
+else:
+    print(y + " is an ok teacher")

42e60ec47af730c8a1efcbaac7bda4e4ca43e58f
annajinn <anna.jinn2@fake.com>
2021-10-26 08:59:27 +1100

    forloop

--- forloop.py
+++ forloop.py
@@ -5,5 +5,5 @@ print("|    Name : Anna                                 |")
 print("|    Version : 01                                |")
 print("|                                                |")
 print(" ------------------------------------------------")
-for x in range(1,499):
+for x in range(1,500):
     print(x)

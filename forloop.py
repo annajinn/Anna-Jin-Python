3719737ec23169f906fe77034d435544c8598bdc
annajinn <anna.jinn2@fake.com>
2021-10-25 21:58:27 +1100

    forloop coding

---
+++ forloop.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    05ForLoop                                   |")
+print("|    Name : Anna                                 |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+for x in range(499):
+    print(x)

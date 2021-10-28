55c6002df4d918628c9c15414596c4c40a304c4d
annajinn <anna.jinn2@fake.com>
2021-10-28 12:12:38 +1100

    h

---
+++ menu.py
@@ -0,0 +1,50 @@
+def x():
+    print("Hello")
+    x=input("What is your name? ")
+    print("Goodbye " + x)
+
+
+
+
+
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    07Menu                                      |")
+print("|    Name : Anna                                 |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("")
+print("1. Hello World")
+print("2. Goodbye World")
+print("3. Goodbye Person")
+print("4. Goodbye Teacher")
+print("5. forLoop")
+print("6. whileLoop")
+print("7. string Loop")
+print("8. convert to ascii")
+print("9. Encode to string")
+print("x. To Exit")
+p=input("Enter an option ")
+print("")
+print("----Start of Output ---------------------------")
+print("")
+print("")
+print("")
+print("")
+print("----End of Output -----------------------------")
+print("")
+print("")
+print("")
+print("Press Enter to Continue")
+
+
+
+
+
+while x():
+    print("")
+    print("hi")
+    print("")
+    x()
+    input('Press enter to continue...')

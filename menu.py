f44021e4c3153327840ca6c52310cbcd878044a5
annajinn <anna.jinn2@fake.com>
2021-11-02 22:26:01 +1100

    menu

--- menu.py
+++ menu.py
@@ -1,10 +1,56 @@
-def x():
+def helloworld():
+    print("Hello world")
+
+def goodbyeworld():
+    print("Hello World")
+    input("------> Program paused - press enter to continue")
+    print("Goodbye World")
+
+def goodbyeperson():
     print("Hello")
     x=input("What is your name? ")
     print("Goodbye " + x)
 
+def goodteacher():
+    x="Mr Horan"
+    y=input("Teacher's name (try Mr Horan) ")
+    if y==x:
+        print("You are lucky, he is a great teacher.")
+    else:
+        print(y + " is an ok teacher")
 
+def forloop():
+    for x in range(1,500):
+        print(x)
 
+def whileloop():
+    x=input("What is the name of this subject ")
+    while x!="IST":
+      print("Not Correct - try again")
+      x=input("What is the name of this subject ")
+    else:
+      print("")
+      print("")
+      print("Congratulations!!")
+      print("")
+      print("")
+      print("")
+
+def start():
+    print("")
+    print("----Start of Output ---------------------------")
+    print("")
+
+def end():
+    print("")
+    print("----End of Output -----------------------------")
+    print("")
+    print("")
+    print("")
+    input("Press Enter to Continue ")
+
+def x():
+    print("no")
 
 
 print(" ------------------------------------------------")
@@ -18,7 +64,7 @@ print("")
 print("1. Hello World")
 print("2. Goodbye World")
 print("3. Goodbye Person")
-print("4. Goodbye Teacher")
+print("4. Good Teacher")
 print("5. forLoop")
 print("6. whileLoop")
 print("7. string Loop")
@@ -26,25 +72,57 @@ print("8. convert to ascii")
 print("9. Encode to string")
 print("x. To Exit")
 p=input("Enter an option ")
-print("")
-print("----Start of Output ---------------------------")
-print("")
-print("")
-print("")
-print("")
-print("----End of Output -----------------------------")
-print("")
-print("")
-print("")
-print("Press Enter to Continue")
 
+while p != "x":
+    start()
+    print("Invalid Option")
+    end()
 
+    if p=="1":
+        start()
+        helloworld()
+        end()
+        
+    if p=="2":
+        start()
+        goodbyeworld()
+        end()
 
+    if p=="3":
+        start()
+        goodbyeperson()
+        end()
 
+    if p=="4":
+        start()
+        goodteacher()
+        end()
 
-while x():
-    print("")
-    print("hi")
-    print("")
-    x()
-    input('Press enter to continue...')
+    if p=="5":
+        start()
+        forloop()
+        end()
+
+    if p=="6":
+        start()
+        whileloop()
+        end()
+
+    if p=="7":
+        start()
+        helloworld()
+        end()
+
+    if p=="8":
+        start()
+        helloworld()
+        end()
+
+    if p=="9":
+        start()
+        helloworld()
+        end()
+
+    if p=="x":
+        start()
+        end()
